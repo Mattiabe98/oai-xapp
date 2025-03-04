f410024fcbbddbc97902af23703d9e29e80547f1
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-04 13:51:53 +0100

    Update xapp.py

--- xapp.py
+++ xapp.py
@@ -185,7 +185,8 @@ for i in range(0, len(conn)):
  ran_type_value = ran_type_ptr.contents
 
 # Print the enum value as an integer (which corresponds to the enum's integer value)
- print(ran_type_value)
+ print(dir(ran_type_ptr))
+ print(type(ran_type_ptr))
  print("Global E2 Node [" + str(i) + "]: PLMN MCC = " + str(conn[i].id.plmn.mcc) + " MNC = " + str(conn[i].id.plmn.mnc) + " Type = " + str(int(conn[i].id.type)))
 
 mac_hndlr = []
