a84da384734e7afe35ae2734138b84baded8a788
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-04 13:14:33 +0100

    Update xapp.py

--- xapp.py
+++ xapp.py
@@ -179,9 +179,8 @@ start_http_server(8000) # Start Prometheus Exporter
 conn = ric.conn_e2_nodes()
 assert(len(conn) > 0)
 for i in range(0, len(conn)):
- print(conn[i].id.type)
- print(dir(conn[i].id.type))
- print(conn[i].ran_func)
+ print(int(conn[i].id.type))
+ print(conn[i].id.type[0])
  print("Global E2 Node [" + str(i) + "]: PLMN MCC = " + str(conn[i].id.plmn.mcc) + " MNC = " + str(conn[i].id.plmn.mnc) + " Type = " + str(conn[i].id.type))
 
 mac_hndlr = []
